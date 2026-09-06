82a6c8ca7ca43aafe4b12a3f629f90d0fd5622ab
MelaMele <melamele06@gmail.com>
2026-09-06 22:04:16 +0300

    Create migration for subjects table

---
+++ database/migrations/2021_01_01_000006_create_subjects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject_name'); // Mathematics, English, Physics, Amharic, Afan Oromo...
+            $table->string('subject_code')->nullable(); // MATH101, ENG01...
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+}
