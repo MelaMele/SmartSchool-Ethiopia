e324607c81b9d819e80160acfd32b9cad8a4f636
MelaMele <melamele06@gmail.com>
2026-09-06 21:51:38 +0300

    Create migration for classes table

---
+++ database/migrations/2021_01_01_000001_create_classes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->id();
+            $table->string('class_label'); // KG 1, KG 2, Grade 1, Grade 2 ... Grade 12
+            $table->integer('priority')->default(1); // ለትክክለኛ ቅደም ተከተል
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes');
+    }
+}
