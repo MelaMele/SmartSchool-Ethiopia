f0a653c5357f4423d82b6dbd6aa85c4ddd4d68b6
MelaMele <melamele06@gmail.com>
2026-09-07 11:07:25 +0300

    Create migration for payment_types table

---
+++ database/migrations/2021_01_01_000017_create_payment_types_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentTypesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('payment_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('payment_type_name'); // ወርሃዊ የትምህርት ክፍያ፣ የምዝገባ ክፍያ፣ የትራንስፖርት፣ የማጠናከሪያ (Tutorial)...
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('payment_types');
+    }
+}
